27efb0fa682c15fd0ed2a12e5d83099dcb1eec65
MrGunner <parsson96@gmail.com>
2023-04-30 13:34:31 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -112,7 +112,7 @@ systemctl restart mariadb &
 wait
 systemctl enable mariadb &
 wait
-link=$(sudo curl -Ls "https://api.github.com/repos/HamedAp/Ssh-User-management/releases/latest" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
+link=$(sudo curl -Ls "https://api.github.com/repos/MGunner/SSH-Clone/releases/latest" | grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')
 sudo wget -O /var/www/html/update.zip $link
 sudo unzip -o /var/www/html/update.zip -d /var/www/html/ &
 wait
